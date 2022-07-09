611af244be35dd4bca01a740e1d33155978a3985
Aristarkh <45267374+AriBram@users.noreply.github.com>
2022-07-09 16:06:59 +0300

    StartDoor logic

--- Assets/Scripts/Doors/StartDoor.cs
+++ Assets/Scripts/Doors/StartDoor.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartDoor : MonoBehaviour
 {
+    Button button;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        button = GetComponent<Button>();
+        anim = GetComponent<Animator>();
+
+       // button.OnClick.AddListner(Open);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Open()
     {
-        
+        anim.Play("Open");
+       // Loader.NextLevel();
     }
+
+
 }
