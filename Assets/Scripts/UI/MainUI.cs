8d6e2f69bae6f02cdc8d095c5c9e6281dba5c151
Aristarkh Abramovsky <ari.bram@ya.ru>
2022-07-09 17:51:09 +0300

    Door prefab logic

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MainUI : MonoBehaviour
 {
+    public KeyCode pauseKey = KeyCode.Escape;
     public GameObject mainPanel;
     public GameObject settPanel;
 
     public static MainUI Instance { get; private set; }
+    enum UIstate { Game, Pause, Setting}
+    UIstate state;
 
     private void Awake()
     {
@@ -16,13 +19,27 @@ public class MainUI : MonoBehaviour
 
     void Start()
     {
-        
+        state = UIstate.Game;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(pauseKey))
+        {
+            switch (state)
+            {
+                case UIstate.Game:
+                    mainPanel.SetActive(true);
+                    state = UIstate.Pause;
+                    break;
+                case UIstate.Pause:
+                case UIstate.Setting:
+                    state = UIstate.Game;
+                    UnPause();
+                    break;
+            }
+        }
     }
 
     public void UnPause()
